12fbd9562ff373a1fdda5028f44001468e745dce
Lam Chan <lamchakchan@gmail.com>
2015-01-25 16:15:34 -0600

    made some version and description changes

--- src/RestAssured/Properties/AssemblyInfo.cs
+++ src/RestAssured/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Scriptcs.RestAssured")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("RestAssured")]
+[assembly: AssemblyDescription("Easy to use REST interface tester")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Scriptcs.RestAssured")]
+[assembly: AssemblyProduct("RestAssured")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
