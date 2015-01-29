9e98ae7e391050b4c7b8891a7817f586d5844c93
Lam Chan <lamchakchan@gmail.com>
2015-01-29 16:16:43 -0600

    nuget build again

--- src/RA.Tests/MockResponseContext.cs
+++ src/RA.Tests/MockResponseContext.cs
@@ -8,24 +8,6 @@ using RestAssured.Tests.Data;
 namespace RA.Tests
 {
     [TestFixture]
-    public class JsonIpIntegration
-    {
-        [Test]
-        public void ResponseShouldShow()
-        {
-            new RestAssured()
-                .Given()
-                    .Name("JsonIP")
-                .When()
-                    .Get("http://www.telize.com/jsonip")
-                .Then()
-                    .Test("ip exist", x => x.ip != null)
-                    .Assert("ip exist");
-        }
-    }
-
-
-    [TestFixture]
     public class MockResponseContext
     {
         private ResponseContext _response;
